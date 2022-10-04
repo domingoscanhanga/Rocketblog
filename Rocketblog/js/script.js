e5bb5f90791b2ddd78a207a4e60d375fad23cfca
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-10-04 16:21:07 +0100

    Menu para small device

--- Rocketblog/js/script.js
+++ Rocketblog/js/script.js
@@ -1,8 +1,7 @@
-const btnMobile = document.querySelector ('#btn-mobile')
-function toggleMenu() {
-    const nav = document.querySelector('#nav')
-    nav.classList.toggle('mostrar-menu')
-    
+const $btnMobile = document.querySelector('#btn-mobile')
+function toggleMenu () {
+    const $nav = document.querySelector('#nav')
+    $nav.classList.toggle('mostrar-menu')
 }
-btnMobile.addEventListener('click', toggleMenu)
+$btnMobile.addEventListener('click', toggleMenu)
 
