92755e97b97567c5abec49c7e8b47809c8328c8f
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-10-05 15:18:57 +0100

    Iniciando o conteúdo da introdução

--- Rocketblog/js/script.js
+++ Rocketblog/js/script.js
@@ -1,7 +1,16 @@
 const $btnMobile = document.querySelector('#btn-mobile')
 function toggleMenu () {
-    const $nav = document.querySelector('#nav')
+    const $nav = document.querySelector('.nav-add')
     $nav.classList.toggle('mostrar-menu')
 }
 $btnMobile.addEventListener('click', toggleMenu)
+
+const fecharMenu = document.querySelector('.menu a')
+function removerMenu (){
+    const $nav = document.querySelector('.nav-remove')
+    $nav.classList.remove('mostrar-menu') 
+}
+fecharMenu.addEventListener('click', removerMenu)
+console.log(fecharMenu)
+
 
