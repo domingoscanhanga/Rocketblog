222f522efa0f9fe6bbaa37fdaca85bcedd139ec4
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-10-06 21:35:15 +0100

    Introdução da secção Intro

--- Rocketblog/js/script.js
+++ Rocketblog/js/script.js
@@ -4,5 +4,5 @@ function activarDesactivarMenu () {
     const $nav = document.querySelector('#nav')
     $nav.classList.toggle('mostrar-menu')
 }
-$btnMenu.addEventListener('click', activarDesactivarMenu)
+$btnMenu.addEventListener('click', activarDesactivarMenu)            
 
