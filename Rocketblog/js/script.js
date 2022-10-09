096fe959ea94024f91e634eabeae6bd6652dc2bd
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-10-09 15:22:19 +0100

    Header  versão mobile concluído, com menu sem acessibilidade.

--- Rocketblog/js/script.js
+++ Rocketblog/js/script.js
@@ -1,2 +1,7 @@
-         
+const $btnMenu = document.querySelector('#btn-menu')   
+function mostarMenuEsconderMenu ()  {
+    const $nav = document.querySelector ('#nav')
+    $nav.classList.toggle('mostar-menu')
+}   
+$btnMenu.addEventListener('click', mostarMenuEsconderMenu) 
 
