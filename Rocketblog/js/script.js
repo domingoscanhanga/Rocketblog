d8dbfd7e6e725cff187b4ec327d570736e8abd58
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-10-09 10:12:13 +0100

    Atualizando o projeto, refazer tudo.

--- Rocketblog/js/script.js
+++ Rocketblog/js/script.js
@@ -1,8 +1,2 @@
-const $btnMenu = document.querySelector('#btn-menu')
-console.log($btnMenu) 
-function activarDesactivarMenu () {
-    const $nav = document.querySelector('#nav')
-    $nav.classList.toggle('mostrar-menu')
-}
-$btnMenu.addEventListener('click', activarDesactivarMenu)            
+         
 
