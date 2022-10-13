03b65c2ab6af66db62c7f2efc7683dae868772d6
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-10-13 22:42:37 +0100

    header incompleto

--- Rocketblog/js/script.js
+++
@@ -1,7 +0,0 @@
-const $btnMenu = document.querySelector('#btn-menu')   
-function mostarMenuEsconderMenu ()  {
-    const $nav = document.querySelector ('#nav')
-    $nav.classList.toggle('mostar-menu')
-}   
-$btnMenu.addEventListener('click', mostarMenuEsconderMenu) 
-
