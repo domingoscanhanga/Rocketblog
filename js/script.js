782cf02ab992cded41e52c702cabed7bd84a5629
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-10-31 11:05:45 +0100

    Atualizando o script do menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 const $btnMenu = document.querySelector ('#btn-menu')
-function toggleMenu () {
+const $Menu = document.querySelector ('#menu')
+
+function handleButtonClick (event) {
 	const $headerNav = document.querySelector ('#header--nav')
+	if(event.type === 'touchstart') event.preventDefault()
+	event.stopPropagation()
+	
 	$headerNav.classList.toggle('MenuMobile')
+	handleClickOutside($Menu, () => {
+		$headerNav.classList.remove('MenuMobile')
+
+	})
 }
-$btnMenu.addEventListener ('click', toggleMenu)
+function handleClickOutside (targetElement, callBack) {
+ const html = document.documentElement
+ function handleHtmlClick (event) {
+	if  (!targetElement.contains(event.target)) {
+		targetElement.removeAttribute('data-target')
+		html.removeEventListener('click',  handleHtmlClick)
+		html.removeEventListener('touchstart', handleHtmlClick)
+		callBack ()
+	}
+ }
+ if (!targetElement.hasAttribute('data-target')) {
+	html.addEventListener('click',  handleHtmlClick)
+	html.addEventListener('touchstart', handleHtmlClick)
+	targetElement.setAttribute('data-target', handleHtmlClick )
+ }
+}
+$btnMenu.addEventListener ('click', handleButtonClick)
+$btnMenu.addEventListener ('touchstart', handleButtonClick)
